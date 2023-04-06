d30e3deee490e74e24796732971a0f03348e14c3
Kento Puleo <kento.puleo@pax.ch>
2023-04-06 14:59:33 +0200

    Azure funktion erstellt um deadline zu loggen

--- TimerTrigger2.cs
+++ TimerTrigger2.cs
@@ -2,16 +2,42 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace Company.Function
 {
     public class TimerTrigger2
     {
         [FunctionName("TimerTrigger2")]
-        public void Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            Console.WriteLine("Hello World");
+            var config = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+
+        var connectionString = config["mysql://root:admin@localhost:3306/mydb"];
+        string sql = "SELECT date FROM list";
+
+         using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                DateTime dateValue = (DateTime)reader["date"];
+                log.LogInformation(dateValue.ToString());
+            }
+
+            reader.Close();
+        }
+        Console.WriteLine("Data retrieval process is complete.");
+
+        
         }
     }
 }
+    
